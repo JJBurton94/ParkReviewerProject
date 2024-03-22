0afbc141ee568930e95f645d91548a3fcf074c26
James Burton <jimbo-rocks@hotmail.co.uk>
2024-03-22 12:52:46 +0000

    basic react homepage

---
+++ park_reviewer/src/footer.js
@@ -0,0 +1,7 @@
+import './headerFooter.css'
+
+export default function Footer(){
+    return(
+        <footer className="footer">Park Reviewer 2024</footer>
+    )
+}
---
+++ park_reviewer/src/heading.js
@@ -0,0 +1,8 @@
+export default function Heading({location = "Worcester"}){
+    return(
+        <div>
+        <h1>Welcome to Park Reviewer</h1>
+        <h2>Lets find something to do in {location}!</h2>
+        </div>
+    )
+}
